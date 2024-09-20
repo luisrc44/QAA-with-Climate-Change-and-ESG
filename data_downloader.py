24367b31cf404db2ffcdb0dff4b2df33dfd4dc71
OscarLVG44 <oscarleonardo080302@gmail.com>
2024-09-19 23:33:01 -0600

    data_downloader modified

--- data_downloader.py
+++ data_downloader.py
@@ -6,27 +6,29 @@ class DataDownloader:
     
     def __init__(self):
         """
-        Initializes the AssetDataDownloader object. This class is designed to download
-        adjusted closing prices for specified financial assets and a benchmark over a given period.
+        Initializes the DataDownloader object. This class is designed to download
+        adjusted closing prices for specified financial assets and a benchmark over a given period,
+        and calculate their monthly returns.
         """
         pass
 
     def download_data(self, start_date: str, end_date: str, assets: list, benchmark: str) -> tuple:
         """
         Downloads adjusted closing prices for a list of assets and a benchmark
-        for the specified period.
+        for the specified period, and calculates their monthly returns.
 
         :param start_date: Start date for data download, in 'YYYY-MM-DD' format.
         :param end_date: End date for data download, in 'YYYY-MM-DD' format.
         :param assets: List of strings with the tickers of the assets to download.
         :param benchmark: String with the ticker of the benchmark to download.
-        :return: A tuple containing two DataFrames: the first with the adjusted closing prices
-                 of the assets and the second with the adjusted closing prices of the benchmark.
+        :return: A tuple containing two DataFrames: the first with the monthly returns
+                 of the assets and the second with the monthly returns of the benchmark.
         """
-        
-        # Download adjusted closing prices for the assets and the given benchmark
         asset_data = yf.download(assets, start=start_date, end=end_date)['Adj Close']
         benchmark_data = pd.DataFrame(yf.download(benchmark, start=start_date, end=end_date)['Adj Close'])
-        benchmark_data = benchmark_data.rename(columns={'Adj Close': benchmark})
+        benchmark_data.columns = [benchmark]
 
-        return pd.DataFrame(asset_data), benchmark_data
+        def calculate_monthly_returns(df):
+            return df.resample('M').last().pct_change().dropna().reset_index().assign(Date=lambda x: x['Date'].apply(lambda y: y.replace(day=1)))
+        
+        return calculate_monthly_returns(asset_data), calculate_monthly_returns(benchmark_data)
